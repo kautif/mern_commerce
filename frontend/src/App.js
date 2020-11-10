855294e89531c0d9cde62e014ab8e54242d2ab28
Autif Kamal <kautif@gmail.com>
2020-11-09 21:25:41 -0700

    added react

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import data from './data';
+
+function App() {
+  return (
+        <div classNameName="grid-container">
+            <header className="row">
+                <div>
+                    <a className="nav-title" href="/">MERN Commerce</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign In</a>
+                </div>
+            </header>
+            <main>
+                <div className="row center">
+                {
+                  data.products.map(product => {
+                    return <div key={product._id} className="card">
+                        <a href={`/product/${product._id}`}>
+                            <img className="medium" src={product.image} alt="product" />
+                        </a>
+                        <a href={`/product/${product._id}`}><h2>{product.name}</h2></a>
+                        <div className="rating">
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                        <div className="price">
+                            ${product.price}
+                        </div> 
+                    </div>
+                    })
+                }
+                </div>
+            </main>
+            <footer className="row center">All Rights Reserved</footer>
+        </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Dress Shirt',
+            category: 'shirts',
+            image: 'images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product'    
+        }, 
+        {
+            _id: '2',
+            name: 'Adidas Dress Shirt',
+            category: 'shirts',
+            image: 'images/p2.jpg',
+            price: 150,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product'    
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Dress Shirt',
+            category: 'shirts',
+            image: 'images/p3.jpg',
+            price: 179,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product'    
+        },
+        {
+            _id: '4',
+            name: 'Nike Dress Pants',
+            category: 'pants',
+            image: 'images/p4.jpg',
+            price: 135,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product'    
+        },
+        {
+            _id: '5',
+            name: 'Adidas Dress Pants',
+            category: 'pants',
+            image: 'images/p5.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product'    
+        },
+        {
+            _id: '6',
+            name: 'Lacoste Dress Pants',
+            category: 'pants',
+            image: 'images/p6.jpg',
+            price: 149,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReview: 10,
+            description: 'high quality product'    
+        }
+    ]
+}
+
+export default data;
